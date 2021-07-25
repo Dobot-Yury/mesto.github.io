cec47f286331007cbddd36033814f1f364d4ae84
Yury Dobot <Dobot23mail@yandex.ru>
2021-07-25 11:55:17 +0300

    main

--- script/script.js
+++ script/script.js
@@ -1,32 +1,28 @@
-const popupOpenButtonElement = document.querySelector('.profile__edit-button')
-const popupOpenCards = document.querySelector('.profile__add-button')
-
-const popupElement = document.querySelector('.popup')
+const popupProfile = document.querySelector('.popup_place_profile')
 const popupCards = document.querySelector('.popup_place_cards')
 const popupImage = document.querySelector('.popup_card_fullscreen')
 
-const popupCloseButtonElement = popupElement.querySelector('.popup__exit-image')
+const popupOpenButtonElement = document.querySelector('.profile__edit-button')
+const popupOpenButtonCards = document.querySelector('.profile__add-button')
+const popupCloseButtonElement = popupProfile.querySelector('.popup__profile-exit')
+const popupCloseButtonCards = popupCards.querySelector('.popup__card-exit')
+const popupCloseButtonImages = popupImage.querySelector('.popup__image-exit')
+
 const profileName = document.querySelector('.profile__title')
 const profileJob = document.querySelector('.profile__subtitle')
-const nameInput = popupElement.querySelector('.popup__input_user_name')
-const jobInput = popupElement.querySelector('.popup__input_user_description')
-const nameCardInput = popupCards.querySelector('.popup__input_image_name')
-const linkInput = popupCards.querySelector('.popup__input_link')
+const nameInputProfile = popupProfile.querySelector('.popup__input_user_name')
+const jobInputProfile = popupProfile.querySelector('.popup__input_user_description')
+const imageInputCard = popupCards.querySelector('.popup__input_image_name')
+const linkInputCard = popupCards.querySelector('.popup__input_link')
 
-const formElements = document.querySelector('.popup__form')
+const formPopupProfile = document.querySelector('.popup__form')
+const formPopupCard = popupCards.querySelector('.popup__card-form')
 
-const cardButtonExit = document.querySelector('.popup__place-exit')
 const cardTemplate = document.querySelector('.template').content
 const cardCase = document.querySelector('.cards')
-const newImageCard = popupCards.querySelector('.popup__input_image_name')
-const newLinkCard = popupCards.querySelector('.popup__input_link')
-const popupElementAdd = popupCards.querySelector('.popup__card-form')
-const cardExit = document.querySelector('.popup__image-exit')
-const buttonElements = document.querySelector('.popup__save')
-const buttonCards = document.querySelector('.popup__save_card')
 
-const errorElement = document.querySelector('.popup__input-error')
-
+const buttonSubmitFormProfile = popupProfile.querySelector('.popup__save_profile')
+const buttonSubmitFormCard = popupCards.querySelector('.popup__save_card')
 
 /*открыть и закрыть окно*/
 
@@ -37,6 +33,7 @@ const openPopup = function(selectPopup){
 
 const closePopup = function(selectPopup){
     selectPopup.classList.remove('popup_is-opened')
+    document.removeEventListener('keydown', closeEsc)
 }
 
 const closePopupByClickOnOverlay = function(event) {
@@ -47,25 +44,25 @@ const closePopupByClickOnOverlay = function(event) {
 }
 
 function openPropfilePopup() {
-    openPopup(popupElement)
-    nameInput.value = profileName.textContent
-    jobInput.value = profileJob.textContent
-    buttonElements.classList.add("popup__save_disabled")
-    checkInputValidity(formElements,nameInput)
-    checkInputValidity(formElements,jobInput)
+    openPopup(popupProfile)
+    nameInputProfile.value = profileName.textContent
+    jobInputProfile.value = profileJob.textContent
+    buttonSubmitFormProfile.classList.add("popup__save_disabled")
+    checkInputValidity(formPopupProfile,nameInputProfile)
+    checkInputValidity(formPopupProfile,jobInputProfile)
 }
 
 function openCardPopup() {
     openPopup(popupCards)
-    buttonCards.classList.add("popup__save_disabled")
-    newLinkCard.value = ""
-    newImageCard.value = ""
+    buttonSubmitFormCard.classList.add("popup__save_disabled")
+    imageInputCard.value = ""
+    linkInputCard.value = ""
 }
 
 function closeCardPopup() {
     closePopup(popupCards)
-    hideInputError(newLinkCard)
-    hideInputError(newImageCard)
+    hideInputError(imageInputCard)
+    hideInputError(linkInputCard)
 } 
 /*Закрытие ESC*/
 
@@ -78,11 +75,11 @@ const closeEsc = (evt) => {
 
 /*Изменение имени и описания*/
 
-function formSubmitHandler (evt) {
+function submitFormProfile (evt) {
     evt.preventDefault()
-    profileName.textContent = nameInput.value
-    profileJob.textContent = jobInput.value
-    closePopup(popupElement)
+    profileName.textContent = nameInputProfile.value
+    profileJob.textContent = jobInputProfile.value
+    closePopup(popupProfile)
 }
 
 /*Карточки*/
@@ -125,21 +122,21 @@ initialCards.forEach(function(el){
 /*Добавление карточки*/
 const addNewCard = function(evt){
     evt.preventDefault()
-    const popupNameCard = newImageCard.value
-    const popupLinkCard = newLinkCard.value
+    const popupNameCard = imageInputCard.value
+    const popupLinkCard = linkInputCard.value
     cardCase.prepend(getCard(popupNameCard, popupLinkCard))
-    newLinkCard.value = ""
-    newImageCard.value = ""
+    imageInputCard.value = ""
+    linkInputCard.value = ""
     closePopup(popupCards)
 }
 
 popupOpenButtonElement.addEventListener('click', openPropfilePopup)
-popupOpenCards.addEventListener('click', openCardPopup)
-cardButtonExit.addEventListener('click', closeCardPopup)
-cardExit.addEventListener('click', () => closePopup(popupImage))
-popupCloseButtonElement.addEventListener('click', () => closePopup(popupElement))
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
+popupOpenButtonCards.addEventListener('click', openCardPopup)
+popupCloseButtonCards.addEventListener('click', closeCardPopup)
+popupCloseButtonImages.addEventListener('click', () => closePopup(popupImage))
+popupCloseButtonElement.addEventListener('click', () => closePopup(popupProfile))
+popupProfile.addEventListener('click', closePopupByClickOnOverlay)
 popupCards.addEventListener('click', closePopupByClickOnOverlay)
 popupImage.addEventListener('click', closePopupByClickOnOverlay)
-popupElementAdd.addEventListener('submit', addNewCard)
-formElements.addEventListener('submit',formSubmitHandler)
+formPopupCard.addEventListener('submit', addNewCard)
+formPopupProfile.addEventListener('submit', submitFormProfile)
